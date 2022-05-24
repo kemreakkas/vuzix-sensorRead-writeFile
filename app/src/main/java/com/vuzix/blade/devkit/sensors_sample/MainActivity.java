2bfee86ff387d3cae1ed4314ff7dcec8e0e02e5c
emre.akkas <kemreakkas@gmail.com>
2022-05-24 11:11:20 +0300

    test_teslim

--- app/src/main/java/com/vuzix/blade/devkit/sensors_sample/MainActivity.java
+++ app/src/main/java/com/vuzix/blade/devkit/sensors_sample/MainActivity.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 import com.vuzix.hud.actionmenu.ActionMenuActivity;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 import android.os.StatFs;
 
@@ -25,17 +27,6 @@ import java.io.BufferedReader;
 
 import java.io.FileReader;
 
-
-/**
- * Main Activity that extend ActionMenuActivity.
- * This main class provide the basic information acquiring data from the device sensors.
- * For more information please reference:
- * https://developer.android.com/guide/topics/sensors/sensors_overview
- * Used Android API Classes:
- * https://developer.android.com/reference/android/hardware/SensorManager
- * https://developer.android.com/reference/android/hardware/SensorEvent
- * https://developer.android.com/reference/android/hardware/SensorEventListener
- */
 public class MainActivity extends ActionMenuActivity implements SensorEventListener {
 
     private final String TAG = "VuzixBDK-Sensors_Sample";
@@ -45,63 +36,110 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
     float accelx,accely,accelz;
     float magnex,magney,magnez;
     float gyrosx,gyrosy,gyrosz;
+    float light,pressure,heartrate;
+    long militemp;
+    FileOutputStream outputStream ;
+    OutputStreamWriter streamWriter ;
+
     //////
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        textArea = (EditText)findViewById(R.id.editText);       ///////
-        /*
-
-        The SensorManager provides information about on device sensors.
-
-        Implementing the MySensorEventListener and overriding the MySensorEventListener.onSensorChanged method, updates to sensors and sensor values can be handled.
-
-        For more information on the SensorManager, MySensorEventListener, Sensor, or SensorEvent classes, please see https://developer.android.com/reference/android/hardware/SensorManager
-
-         */
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        samplexyzFile = new File(Environment.getExternalStorageDirectory() + "/samplexyz-file.csv");
-        listDevices();
+    protected void onDestroy() {
+        try {
+            outputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        super.onDestroy();
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textArea = (EditText)findViewById(R.id.editText);
+        try {
+            String fileNameString = Calendar.getInstance().getTime().toString();
+            Log.d(TAG, "Time: ?? " + Calendar.getInstance().getTime().toString());
+            fileNameString = fileNameString.replaceAll(":","_");
+            Log.d(TAG, "Replaced Time: ?? " + fileNameString);
+             samplexyzFile = new File(Environment.getExternalStorageDirectory()+
+                    "/"+fileNameString+".csv");
+
+            // samplexyzFile = new File(Environment.getExternalStorageDirectory()+"/asdfeq.csv");
+            Log.d(TAG, "Data Path: " + Environment.getExternalStorageDirectory()+
+                    "/"+fileNameString+".csv");
+            samplexyzFile.createNewFile();
+            Thread.sleep(3000);
+            outputStream = new FileOutputStream(samplexyzFile);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.e(TAG, "EXCEPTION: " + e);
+
+        }
+        streamWriter = new OutputStreamWriter(outputStream);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Time"+";");
+        builder.append("Millisecond"+";");
+        builder.append("Accelerometer X"+";");
+        builder.append("Accelerometer Y"+";");
+        builder.append("Accelerometer Z"+";");
+        builder.append("GYROSCOPE X"+";");
+        builder.append("GYROSCOPE Y"+";");
+        builder.append("GYROSCOPE Z"+";");
+        builder.append("MAGNETOMETER X"+";");
+        builder.append("MAGNETOMETER Y"+";");
+        builder.append("MAGNETOMETER Z"+";");
+        builder.append("LIGHT"+";");
+        builder.append("PRESSURE"+";");
+
+        builder.append("\n");
+
+        try {
+            streamWriter.write(builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            streamWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        listDevices();
+    }
 
     //Registration of Event Listeners should be done here for performance.
     @Override
     protected void onResume() {
         super.onResume();
-        logStorageInfo();                       ///////
-
-        // register this class as a listener for the gyroscope sensor
+       // logStorageInfo();                       
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
                 SensorManager.SENSOR_DELAY_NORMAL);
-
         // register this class as a listener for the gyroscope sensor
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
-
-
         // register this class as a listener for the gyroscope sensor
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
                 SensorManager.SENSOR_DELAY_NORMAL);
-
         // register this class as a listener for the gyroscope sensor
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
                 SensorManager.SENSOR_DELAY_NORMAL);
-
         // register this class as a listener for the gyroscope sensor
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE),
                 SensorManager.SENSOR_DELAY_NORMAL);
-
         if (isExternalStorageWritable()) {
             textArea.append("Attempting to Write to storage \n");
-            this.writeFileToExternalStorage();
+           // this.writeFileToExternalStorage();
+            this.sensorYazdir(streamWriter);
         } else {
             Log.e(TAG, "Cannot write to external storage");
             textArea.setText("");
@@ -123,28 +161,7 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
         }
 
     }
-    private void logStorageInfo() {
-        Log.d(TAG, "Location: " + Environment.getExternalStorageDirectory());
-        Log.d(TAG, "State: " + Environment.getExternalStorageState());
-        Log.d(TAG, "Removable: " + Environment.isExternalStorageRemovable(samplexyzFile));
-        Log.d(TAG, "Emulated: " + Environment.isExternalStorageEmulated(samplexyzFile));
-        Log.d(TAG, "Path: " + Environment.getExternalStorageDirectory().getAbsolutePath());
-        Log.d(TAG, "SD mount state: " + isSdMounted());
-        Log.d(TAG, "Internal Space: " + getAvailableSpaceInMBInternal() + " MB");
-        if (isSdMounted()) {
-            Log.d(TAG, "SD card free space: " + getAvailableSpaceInMbSD() + " MB");
-        }
 
-        textArea.append("Location: " + Environment.getExternalStorageDirectory() + "\n");
-        //textArea.append("State: " + Environment.getExternalStorageState() + "\n");
-        //textArea.append("Removable: " + Environment.isExternalStorageRemovable(samplexyzFile)  + "\n");
-        //textArea.append("Emulated: " + Environment.isExternalStorageEmulated(samplexyzFile)  + "\n");
-        textArea.append("Internal Space: " + getAvailableSpaceInMBInternal() + " MB"  + "\n");
-        textArea.append("SD card mounted: " +  isSdMounted() + "\n");
-        if (isSdMounted()) {
-            textArea.append("SD card free space: " + getAvailableSpaceInMbSD() + " MB" + "\n");
-        }
-    }
     public static long getAvailableSpaceInMBInternal(){
         final long SIZE_KB = 1024L;
         final long SIZE_MB = SIZE_KB * SIZE_KB;
@@ -179,18 +196,15 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
         return (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
 
     }
-    private void writeFileToExternalStorage() {
+  /*  private void writeFileToExternalStorage() {
 
         try {
-
             samplexyzFile.createNewFile();
-
             FileOutputStream outputStream = new FileOutputStream(samplexyzFile);
             OutputStreamWriter streamWriter = new OutputStreamWriter(outputStream);
-            streamWriter.append("VUZIX BLADE");
+            //streamWriter.append("VUZIX BLADE");
 
             streamWriter.close();
-
             outputStream.flush();
             outputStream.close();
 
@@ -200,21 +214,30 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
             textArea.append("ERROR: \n");
             textArea.append("Error writing to file: " + ex.toString());
         }
-    }
-    private void sensoryazdir(){
+    }*/
+    private void sensorYazdir(OutputStreamWriter streamWriter){
         try {
+            militemp=(Calendar.getInstance().getTimeInMillis());
+            StringBuilder builder = new StringBuilder();
+            builder.append(String.valueOf( Calendar.getInstance().getTime().toString())+";");
+            //builder.append(String.valueOf((Calendar.getInstance().getTimeInMillis()))+";");
+            builder.append(String.valueOf( militemp)+";");
+            builder.append(String.valueOf( accelx)+";");
+            builder.append(String.valueOf( accely)+";");
+            builder.append(String.valueOf( accelz)+";");
+            builder.append(String.valueOf( gyrosx)+";");
+            builder.append(String.valueOf( gyrosy)+";");
+            builder.append(String.valueOf( gyrosz)+";");
+            builder.append(String.valueOf( magnex)+";");
+            builder.append(String.valueOf( magney)+";");
+            builder.append(String.valueOf( magnez)+";");
+            builder.append(String.valueOf( light)+";");
+            builder.append(String.valueOf(pressure)+";");
 
-            FileOutputStream outputStream = new FileOutputStream(samplexyzFile);
-            OutputStreamWriter streamWriter = new OutputStreamWriter(outputStream);
-            streamWriter.append("VUZIX BLADE a ");
-            streamWriter.append("test");
-            streamWriter.append((char) accelx);
-            streamWriter.append((char) accely);
-            streamWriter.append((char) accelz);
+            builder.append("\n");
 
-            streamWriter.close();
-            outputStream.flush();
-            outputStream.close();
+            streamWriter.write(builder.toString());
+            streamWriter.flush();
 
         } catch (Exception ex) {
             Log.e(TAG, "Error writing to file: " + ex.toString());
@@ -224,8 +247,6 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
         }
     }
 
-
-    //Remember to always remove the listeners that you don't need to reduce leakage and battery usage
     @Override
     protected void onPause() {
         super.onPause();
@@ -233,15 +254,9 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
         sensorManager.unregisterListener(this);
     }
 
-    /**
-     * Simply get a list of sensor from the SensorManager and loop over the list, logging basic information about each sensor
-     */
     private void listDevices() {
-
         List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
-
-
-        for (Sensor sensor : sensorList) {
+       /* for (Sensor sensor : sensorList) {
             Log.d(TAG, "====================================================================================================");
             Log.d(TAG, "Name: \t\t\t\t" + sensor.getName());
             Log.d(TAG, "Vendor: \t\t\t" + sensor.getVendor());
@@ -250,31 +265,21 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
             Log.d(TAG, "Resolution: \t\t" + Float.toString(sensor.getResolution()));
             Log.d(TAG, "Power: \t\t\t\t" + Float.toString(sensor.getPower()));
             Log.d(TAG, "Minimum Display: \t" + Integer.toString(sensor.getMinDelay()));
-        }
-
+        }*/
     }
 
-    /*
-
-    When information about the gyroscope changes, log the new values
-
-     */
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-
-        Log.d(TAG, sensorEvent.sensor.toString());
-
+        //Log.d(TAG, sensorEvent.sensor.toString());
         switch (sensorEvent.sensor.getStringType()){
-
             case Sensor.STRING_TYPE_ACCELEROMETER:
                 ((TextView)findViewById(R.id.sensor_accel_data_textView)).setText(String.valueOf(sensorEvent.values[0]));
-                Log.d(TAG, "Acceleration minus Gx on the x-axis: " + sensorEvent.values[0]);
+        //        Log.d(TAG, "Acceleration minus Gx on the x-axis: " + sensorEvent.values[0]);
                 accelx=sensorEvent.values[0];
-
                 if (sensorEvent.values.length > 1)
                 {
-                    Log.d(TAG, "Acceleration minus Gy on the y-axis: " + sensorEvent.values[1]);
-                    Log.d(TAG, "Acceleration minus Gz on the z-axis: " + sensorEvent.values[2]);
+        //            Log.d(TAG, "Acceleration minus Gy on the y-axis: " + sensorEvent.values[1]);
+        //            Log.d(TAG, "Acceleration minus Gz on the z-axis: " + sensorEvent.values[2]);
                     ((TextView)findViewById(R.id.sensor_accel_data_textView2)).setText(String.valueOf(sensorEvent.values[1]));
                     ((TextView)findViewById(R.id.sensor_accel_data_textView3)).setText(String.valueOf(sensorEvent.values[2]));
                     accely=sensorEvent.values[1];
@@ -283,13 +288,12 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
                 break;
             case Sensor.STRING_TYPE_MAGNETIC_FIELD:
                 ((TextView)findViewById(R.id.sensor_mag_data_textView)).setText(String.valueOf(sensorEvent.values[0]));
-                Log.d(TAG, "Magnetometer Data: " + sensorEvent.values[0]);
+              //  Log.d(TAG, "Magnetometer Data: " + sensorEvent.values[0]);
                 magnex=sensorEvent.values[0];
-
                 if (sensorEvent.values.length > 1)
                 {
-                    Log.d(TAG, "Magnetometer Data2: " + sensorEvent.values[1]);
-                    Log.d(TAG, "Magnetometer Data:3 " + sensorEvent.values[2]);
+                //    Log.d(TAG, "Magnetometer Data2: " + sensorEvent.values[1]);
+                //    Log.d(TAG, "Magnetometer Data:3 " + sensorEvent.values[2]);
                     ((TextView)findViewById(R.id.sensor_mag_data_textView2)).setText(String.valueOf(sensorEvent.values[1]));
                     ((TextView)findViewById(R.id.sensor_mag_data_textView3)).setText(String.valueOf(sensorEvent.values[2]));
                     magney=sensorEvent.values[1];
@@ -298,75 +302,58 @@ public class MainActivity extends ActionMenuActivity implements SensorEventListe
                 break;
             case Sensor.STRING_TYPE_GYROSCOPE:
                 ((TextView)findViewById(R.id.sensor_gyro_data_textView)).setText(String.valueOf(sensorEvent.values[0]));
-                Log.d(TAG, "GYROSCOPE minus Gx on the x-axis: " + sensorEvent.values[0]);
+            //    Log.d(TAG, "GYROSCOPE minus Gx on the x-axis: " + sensorEvent.values[0]);
                 gyrosx=sensorEvent.values[0];
                 if (sensorEvent.values.length > 1)
                 {
-                    Log.d(TAG, "GYROSCOPE minus Gy on the y-axis: " + sensorEvent.values[1]);
-                    Log.d(TAG, "GYROSCOPE minus Gz on the z-axis: " + sensorEvent.values[2]);
+            //        Log.d(TAG, "GYROSCOPE minus Gy on the y-axis: " + sensorEvent.values[1]);
+            //        Log.d(TAG, "GYROSCOPE minus Gz on the z-axis: " + sensorEvent.values[2]);
                     ((TextView)findViewById(R.id.sensor_gyro_data_textView2)).setText(String.valueOf(sensorEvent.values[1]));
                     ((TextView)findViewById(R.id.sensor_gyro_data_textView3)).setText(String.valueOf(sensorEvent.values[2]));
-                    gyrosx=sensorEvent.values[1];
-                    gyrosx=sensorEvent.values[2];
+                    gyrosy=sensorEvent.values[1];
+                    gyrosz=sensorEvent.values[2];
                 }
                 break;
             case Sensor.STRING_TYPE_LIGHT:
                 ((TextView)findViewById(R.id.sensor_light_data_textView)).setText(String.valueOf(sensorEvent.values[0]));
                 Log.d(TAG, "Ambient Light Data: " + sensorEvent.values[0]);
+                light=sensorEvent.values[0];
 
                 break;
             case Sensor.STRING_TYPE_PRESSURE:
                 ((TextView)findViewById(R.id.sensor_press_data_textView)).setText(String.valueOf(sensorEvent.values[0]));
-                Log.d(TAG, "Pressure/Barometer Data: " + sensorEvent.values[0]);
-
+            //    Log.d(TAG, "Pressure/Barometer Data: " + sensorEvent.values[0]);
+                pressure=sensorEvent.values[0];
+                Log.d(TAG, Calendar.getInstance().getTime().toString());
+                Log.d(TAG, String.valueOf((Calendar.getInstance().getTimeInMillis())));
                 break;
-
         }
 
-
-        sensoryazdir();
+        sensorYazdir(streamWriter);
     }
 
-    /*
-
-    This is a simple example of logging accuracy changes to a sensor
-
-     */
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-        Log.d(TAG, sensor.toString());
+
     }
     private void readFileFromExternalStorage() {
-
         String fileContent = "";
-
         if (samplexyzFile.exists()) {
-
             BufferedReader reader;
-
             try {
-
                 reader = new BufferedReader(new FileReader(samplexyzFile));
-
                 String line;
-
                 while ((line = reader.readLine()) != null) {
-
                     fileContent += line;
-
                 }
-
                 reader.close();
-
             } catch (Exception ex) {
-
                 Log.e(TAG, "Error reading from file: " + ex.toString());
                 textArea.setText("");
                 textArea.append("ERROR: \n");
                 textArea.append("Error reading from file: " + ex.toString());
             }
         }
-
         Log.d(TAG, "File contents: " + fileContent);
         textArea.append("File contents: " + fileContent + "\n");
 
